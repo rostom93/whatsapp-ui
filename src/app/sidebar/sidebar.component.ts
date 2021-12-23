530f4f835e0f98adf7539c66cbf81d2485e5424a
rno.reflow <rno@re-flow.io>
2021-12-23 23:57:07 +0100

    whatsapp ui static

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,198 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent implements OnInit {
+  @Output() conversationClicked: EventEmitter<any> = new EventEmitter();
+  searchText: string;
+  conversations = [
+    {
+      name: 'Francois Hollande',
+      time: '8:21',
+      latestMessage: 'Hi there!!',
+      latestMessageRead: false,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'jacques chirac',
+      time: '8:21',
+      latestMessage: 'wow',
+      latestMessageRead: true,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Andrew',
+      time: '8:21',
+      latestMessage: 'I am fine',
+      latestMessageRead: false,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'emmanuel macron',
+      time: '8:21',
+      latestMessage: 'lol',
+      latestMessageRead: true,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Vladimir Putin',
+      time: '8:21',
+      latestMessage: 'Alright',
+      latestMessageRead: false,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'donald Trump',
+      time: '8:21',
+      latestMessage: "Let's go",
+      latestMessageRead: false,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Tom cruz',
+      time: '8:21',
+      latestMessage: 'I see',
+      latestMessageRead: true,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Zlatan Ibrahimovic',
+      time: '8:21',
+      latestMessage: 'OMG',
+      latestMessageRead: false,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Nicolas Laurant',
+      time: '8:21',
+      latestMessage: 'Oh No',
+      latestMessageRead: false,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Cristiano Ronaldo',
+      time: '8:21',
+      latestMessage: 'Thanks',
+      latestMessageRead: true,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Rostom Nouisser',
+      time: '8:21',
+      latestMessage: 'Take care',
+      latestMessageRead: false,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Monna',
+      time: '8:21',
+      latestMessage: 'I am coming now',
+      latestMessageRead: false,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Thamer',
+      time: '8:21',
+      latestMessage: 'Good Morning!',
+      latestMessageRead: true,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+    {
+      name: 'Salim',
+      time: '8:21',
+      latestMessage: 'Good Morning!',
+      latestMessageRead: true,
+      messages: [
+        { id: 1, body: 'Hello world', time: '8:21', me: true },
+        { id: 2, body: 'How are you?', time: '8:21', me: false },
+        { id: 3, body: 'I am fine thanks', time: '8:21', me: true },
+        { id: 4, body: 'Glad to hear that', time: '8:21', me: false },
+      ],
+    },
+  ];
+
+  get filteredConversations() {
+    return this.conversations.filter((conversation) => {
+      return (
+        conversation.name
+          .toLowerCase()
+          .includes(this.searchText.toLowerCase()) ||
+        conversation.latestMessage
+          .toLowerCase()
+          .includes(this.searchText.toLowerCase())
+      );
+    });
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
